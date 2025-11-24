e8a03b1e58758412cc5fa96c47c24947bab7dc68
nechmerust <141737869+nechmerust@users.noreply.github.com>
2025-11-24 12:48:20 +0100

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'csop-trosecnici-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './about.html',
+  './locations.html',
+  './projects.html',
+  './karel.html',
+  './letter.html',
+  './contact.html',
+  './styles.css',
+  './script.js',
+  './images/logo.png',
+  './favicon.ico'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
